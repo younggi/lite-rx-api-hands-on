56324a8d95405382483ee16150d5fb1a4e63981b
Younggi Kim <roadlamp@gmail.com>
2016-12-23 16:47:40 +0900

    Add Part 01 Flux solved

--- src/test/java/io/pivotal/literx/Part01Flux.java
+++ src/test/java/io/pivotal/literx/Part01Flux.java
@@ -2,9 +2,13 @@ package io.pivotal.literx;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.test.StepVerifier;
 
 /**
@@ -28,7 +32,7 @@ public class Part01Flux {
 
 	// TODO Return an empty Flux
 	Flux<String> emptyFlux() {
-		return null;
+		return Flux.empty();
 	}
 
 //========================================================================================
@@ -44,7 +48,12 @@ public class Part01Flux {
 
 	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
 	Flux<String> fooBarFluxFromValues() {
-		return null;
+
+		return Flux.create(s -> {
+      s.next("foo");
+      s.next("bar");
+      s.complete();
+    });
 	}
 
 //========================================================================================
@@ -60,7 +69,8 @@ public class Part01Flux {
 
 	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
 	Flux<String> fooBarFluxFromList() {
-		return null;
+
+		return Flux.fromIterable(Arrays.asList("foo", "bar"));
 	}
 
 //========================================================================================
@@ -74,7 +84,7 @@ public class Part01Flux {
 	}
 	// TODO Create a Flux that emits an IllegalStateException
 	Flux<String> errorFlux() {
-		return null;
+		return Flux.create(s -> s.error(new IllegalStateException()));
 	}
 
 //========================================================================================
@@ -90,7 +100,10 @@ public class Part01Flux {
 
 	// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
 	Flux<Long> counter() {
-		return null;
+		return Flux.<Long>create(s -> {
+      Stream.iterate(0L, l -> l + 1).limit(10).forEach(l -> s.next(l));
+      s.complete();
+    }).delay(Duration.ofMillis(100));
 	}
 
 }
